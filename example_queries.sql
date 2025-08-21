2f252992f85b0c65c24002e035fd656399acbb73
Garytech64 <garytech4@outlook.com>
2025-08-21 12:03:06 +0100

    Update example_queries.sql
    Added Query 2 (top 5 most common incident types)

--- example_queries.sql
+++ example_queries.sql
@@ -1,3 +1,11 @@
--- Count total number of incidents
-SELECT COUNT(*) AS Total_Incidents
+-- Query 1: Count how many records are in the traffic incidents table
+SELECT COUNT(*) AS TotalIncidents
 FROM Real_Time_Traffic_Incident_Reports;
+
+-- Query 2: Find the top 5 most common incident types
+SELECT TOP 5 
+    Issue_Reported, 
+    COUNT(*) AS Total
+FROM Real_Time_Traffic_Incident_Reports
+GROUP BY Issue_Reported
+ORDER BY Total DESC;
